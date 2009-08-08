e3e88f76857cad190c28082a69df5e06524d2920
CrociDB <crocidb@gmail.com>
2009-08-08 00:53:01 -0300

    Adding the CArm Class

---
+++ include/CArm.h
@@ -0,0 +1,38 @@
+/**
+ * GameJam 2009 - GamePlay
+ *
+ * The Arm class file.
+ */
+ 
+ #ifndef _CARM_H_
+ #define _CARM_H_
+ 
+ #define DEFAULT_X		0
+ #define DEFAULT_Y		0
+ 
+ #include "CMap.h"
+ #include "AnimSprite.h"
+ 
+ class CArm
+ {
+ private:
+	int x, y;
+	
+	AnimSprite *sprite;
+	
+ public:
+	CArm();
+	
+	// Setters
+	void setX(int px);
+	void setY(int py);
+	
+	// Getters
+	int getX();
+	int getY();
+ };
+ 
+ 
+ 
+ #endif
+ 
