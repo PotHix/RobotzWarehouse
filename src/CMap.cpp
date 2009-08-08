70423e91cd178cb311a8e84de3d05d960a5a19fb
PotHix <willian.molinari@gmail.com>
2009-08-08 00:00:54 -0300

    Creating cpp file to map

---
+++ src/CMap.cpp
@@ -0,0 +1,35 @@
+/*
+ * 
+ * 
+ *
+ * 
+ *
+ *
+ *
+ */
+
+#include <CMap.h>
+
+CMap::CMap(){
+   setWidth(2);
+   setHeight(2);
+   initMatrix();
+}
+
+void CMap::setWidth(int w){
+   width = w;
+}
+
+void CMap::setHeight(int h){
+   height = h;
+}
+
+void CMap::initMatrix(){
+   items = EItem*[width]
+   for(int i=0; i < width; i++){
+      items[i] = new EItem[height];
+      for(int j=0; j < height; j++){
+         items[i][j] = EITEM_NULL;
+      }
+   }
+}
